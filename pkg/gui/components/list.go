b26ce8123390a9be2dfb6fba056a1dfe5340e09d
Gianni Massi <me@giannimassi.it>
2019-09-17 14:14:06 +0200

    gui/components: support navigation of list's cards from layout

--- pkg/gui/components/list.go
+++ pkg/gui/components/list.go
@@ -1,12 +1,17 @@
 package components
 
 import (
+	"bytes"
 	"fmt"
-	"strings"
+	"math"
 
 	"github.com/giannimassi/trello-tui/pkg/gui/panel"
 	"github.com/jroimartin/gocui"
 	"github.com/pkg/errors"
+)
+
+const (
+	cardContentHeight = 6
 )
 
 type List struct {
@@ -31,15 +36,36 @@ func (l *List) Draw(g *gocui.Gui, ctx *Context) error {
 	if ctx.IsListSelected(l.idx) {
 		l.View, _ = g.SetCurrentView(l.Name())
 	}
-	w, _ := l.Size()
+	w, h := l.Size()
 	l.Panel.Clear()
 	if !l.visible {
 		return nil
 	}
-	_, _ = l.Panel.Write([]byte("\n\n" + strings.Repeat("-", w-1)))
-	for _, id := range ctx.ListCardsIds(l.idx) {
-		_, _ = ctx.Color(CardTitleClass, ctx.IsCardSelected(id)).Fprint(l.Panel, ctx.CardTitle(id))
-		_, _ = l.Panel.Write([]byte("\n\n" + strings.Repeat("-", w-1)))
+
+	var (
+		cardsPerPage = h / cardContentHeight
+		cardIds      = ctx.ListCardsIds(l.idx)
+	)
+	ctx.UpdateFirstCardIndex(cardsPerPage+1, cardIds)
+	firstCardIdx := ctx.FirstCardIndex(l.idx)
+
+	_, _ = l.Panel.Write([]byte("\n\n"))
+	for i, id := range cardIds {
+		if i < firstCardIdx || i > firstCardIdx+cardsPerPage {
+			continue
+		}
+
+		n, title := prepTitle(ctx.CardTitle(id), w-1, cardContentHeight-2)
+		_, _ = ctx.Color(CardTitleClass, ctx.IsCardSelected(id)).Fprint(l.Panel, title)
+		_, _ = l.Panel.Write(bytes.Repeat([]byte("\n"), cardContentHeight-n-1))
+		_, _ = l.Panel.Write(bytes.Repeat([]byte("-"), w-1))
 	}
 	return nil
+}
+
+func prepTitle(title string, w, l int) (int, string) {
+	if len(title) > w*(l-1) {
+		return l, title[:w*(l-1)] + "..."
+	}
+	return int(math.Ceil(float64(len(title)) / float64(w))), title
 }
